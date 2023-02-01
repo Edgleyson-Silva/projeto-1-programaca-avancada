04d4e45f866aa3cfb309144e210ce7561329cab1
Felipe Lyra Mororo <70602331+felipemororo@users.noreply.github.com>
2023-02-01 18:40:27 -0300

    Add files via upload

--- projeto1.cpp
+++ projeto1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 using namespace std;
 
 class Ambiente{
@@ -38,9 +39,27 @@ public:
 
     }
 
+    void AdicionarRetangulo(){
+      int x1, y1, x2, y2;
+      x1 = 0;
+      x2 = 3;
+      y1 = 0;
+      y2 = 3;
+      for(int i = x1; i <= x2; i++){
+        for(int j = y1; j <= y2; j++){
+          if (grade[i][j] == 'E'){
+            cout << "Nao eh possivel adicionar um obstaculo retangular no local";
+          }
+          else{
+            grade[i][j] = '1';
+          }
+        }
+      }
+    }
+
     void ImprimirAmbiente(){
         cout << "Ambiente: " << endl;
-        for (int i = 0; i < dimensao_x; i++){
+        for (int i = 0; i < dimensao_x; i++){cout << "Nao eh possivel adicionar um obstaculo no local";
             for (int j = 0; j < dimensao_y; j++){
                 cout << grade[i][j] << " ";
             }
@@ -50,7 +69,21 @@ public:
 };
 
 int main(){
-    Ambiente ambienteTeste(10,6,2,3);
+    string coord[100];
+    string line;
+    ifstream myfile("example.txt");
+    int i = 0;
+    if (myfile.is_open()){
+      while(getline(myfile,line)){
+        coord[i] = line;
+        i++;
+      }
+    }
+    for(i=0; i<4; i++){
+      cout << coord[i] << '\n';
+    }
+    Ambiente ambienteTeste(stoi(coord[0]),stoi(coord[1]),stoi(coord[2]),stoi(coord[3]));
+    ambienteTeste.AdicionarRetangulo();
     ambienteTeste.ImprimirAmbiente();
     return 0;
 }
